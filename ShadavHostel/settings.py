09429f09cf9e0be57ab8391d661cedfe16a83623
jim-les <84589373+jim-les@users.noreply.github.com>
2024-06-17 11:28:53 +0300

    Update the website pages

--- ShadavHostel/settings.py
+++ ShadavHostel/settings.py
@@ -131,3 +131,4 @@ LOGOUT_REDIRECT_URL = 'login_view'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+CSRF_TRUSTED_ORIGINS=['https://shadav.onrender.com', 'http://127.0.0.1:8000']
